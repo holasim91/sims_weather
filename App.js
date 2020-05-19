744a052dd60272d9aa9d5ab90b357f0a62deb649
HyunIn Sim <>
2020-05-19 18:34:24 +0900

    connect openWeatherAPI

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Alert } from "react-native";
+import axios from 'axios'
+import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const API_KEY = '3e7525ebfa8f979aba88cb2fd0d0a435'
+
+export default class extends React.Component {
+  state={
+    isLoading: true
+  }
+  getWeather = async(latitude, longitude) =>{
+    const {data} = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+      console.log('???',data)
+  }
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync();
+      // API에 요청을 해서 날씨정보 받아옴
+      this.getWeather(latitude, longitude)
+      this.setState({isLoading:false})
+    } catch (error) {
+      Alert.alert("Can't find you", "so sad..");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const {isLoading} = this.state;
+    return isLoading? <Loading />: null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
